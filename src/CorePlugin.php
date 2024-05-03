cec6ac5e547ee0e1a6765fd7b2f58dde8777f82f
Sorethea OK <sorethea@gmail.com>
2024-05-03 10:52:19 +0700

    Update user resource

--- src/CorePlugin.php
+++ src/CorePlugin.php
@@ -2,8 +2,11 @@
 
 namespace Sorethea\Core;
 
+use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Contracts\Plugin;
+use Filament\Forms\Components\FileUpload;
 use Filament\Panel;
+use Jeffgreco13\FilamentBreezy\BreezyCore;
 use Sorethea\Core\Resources\RoleResource;
 use Sorethea\Core\Resources\UserResource;
 
@@ -22,7 +25,17 @@ class CorePlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        $panel->resources([
+        $panel->plugins([
+            FilamentShieldPlugin::make(),
+            BreezyCore::make()
+                ->myProfile(
+                    hasAvatars: true,
+                )
+                ->avatarUploadComponent(fn() => FileUpload::make('avatar_url')
+                    ->avatar()
+                    ->directory('profile-photos')
+                    ->disk('public')),
+        ])->resources([
             UserResource::class,
             RoleResource::class,
         ]);
